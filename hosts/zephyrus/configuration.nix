28714050b2cb0313ac4213b4b9f41201d82d0df9
Matthew Toohey <contact@mtoohey.com>
2022-04-06 22:34:25 -0400

    add kernel patches for g14

--- hosts/zephyrus/configuration.nix
+++ hosts/zephyrus/configuration.nix
@@ -9,6 +9,11 @@ let
       "https://github.com/NixOS/nixpkgs/archive/a4a81b6f6c27e5a964faea25b7b5cbe611f98691.tar.gz";
     sha256 = "1z9j1hp69i3j8b3v9val8v4sxy7hzdggg2a2rfvjp7aq6h1bpfax";
   };
+  g14_patches = fetchGit {
+    url = "https://gitlab.com/dragonn/linux-g14";
+    ref = "5.17";
+    rev = "ed8cf277690895c5b2aa19a0c89b397b5cd2073d";
+  };
 in
 {
   imports = [
@@ -40,9 +45,36 @@ in
     gfx-vfio-enable = true;
   };
   services.asusctl.enable = true;
+  environment.etc."asusd/profile.conf".text = "active_profile = \"Quiet\"";
 
-  # TODO: add kernel patches to get asusctl profiles working as per https://gitlab.com/asus-linux/asusctl/-/issues/134
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_5_17;
+  boot.kernelPatches = map (patch: { inherit patch; }) [
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-0004-5.15+--more-uarches-for-kernel.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-0005-lru-multi-generational.patch"
+
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-0043-ALSA-hda-realtek-Fix-speakers-not-working-on-Asus-Fl.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-0047-asus-nb-wmi-Add-tablet_mode_sw-lid-flip.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-0048-asus-nb-wmi-fix-tablet_mode_sw_int.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-0049-ALSA-hda-realtek-Add-quirk-for-ASUS-M16-GU603H.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-0050-asus-flow-x13-support_sw_tablet_mode.patch"
+
+    # mediatek mt7921 bt/wifi patches
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-8017-mt76-mt7921-enable-VO-tx-aggregation.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-8018-mt76-mt7921e-fix-possible-probe-failure-after-reboot.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-8026-cfg80211-dont-WARN-if-a-self-managed-device.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-8050-r8152-fix-spurious-wakeups-from-s0i3.patch"
+
+    # squashed s0ix enablement through
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-9001-v5.16.11-s0ix-patch-2022-02-23.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-9004-HID-asus-Reduce-object-size-by-consolidating-calls.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-9005-acpi-battery-Always-read-fresh-battery-state-on-update.patch"
+
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-9006-amd-c3-entry.patch"
+
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-9010-ACPI-PM-s2idle-Don-t-report-missing-devices-as-faili.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-9011-cpufreq-CPPC-Fix-performance-frequency-conversion.patch"
+    "${g14_patches}/sys-kernel_arch-sources-g14_files-9012-Improve-usability-for-amd-pstate.patch"
+  ];
 
   # for quiet startup
   boot.consoleLogLevel = 0;

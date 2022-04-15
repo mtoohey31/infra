da7c3dde43422a4270c3ec1ce17523093f364e87
Matthew Toohey <contact@mtoohey.com>
2022-04-14 23:10:53 -0400

    set a bunch of darwin defaults

--- darwin/modules/common.nix
+++ darwin/modules/common.nix
@@ -7,4 +7,34 @@
   };
 
   services.nix-daemon.enable = true;
+
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyleSwitchesAutomatically = true;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSDocumentSaveNewDocumentsToCloud = false;
+      _HIHideMenuBar = true;
+    };
+    dock = {
+      autohide = true;
+      autohide-delay = "0.0";
+      autohide-time-modifier = "0.25";
+      static-only = true;
+    };
+    finder = {
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      CreateDesktop = false;
+      FXEnableExtensionChangeWarning = false;
+      FXPreferredViewStyle = "clmv";
+      ShowPathbar = true;
+    };
+    loginwindow.GuestEnabled = false;
+    trackpad = {
+      Clicking = true;
+      Dragging = true;
+      TrackpadThreeFingerDrag = true;
+    };
+  };
 }

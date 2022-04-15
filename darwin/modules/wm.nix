48247ab94e47c0719fecfe43346b60999f31cf95
Matthew Toohey <contact@mtoohey.com>
2022-04-14 23:12:24 -0400

    add darwin wm module

---
+++ darwin/modules/wm.nix
@@ -0,0 +1,60 @@
+{ pkgs, ... }:
+
+{
+  services = {
+    skhd = {
+      enable = true;
+      skhdConfig = ''
+        cmd - h : yabai -m window --focus west
+        cmd - j : yabai -m window --focus south
+        cmd - k : yabai -m window --focus north
+        cmd - l : yabai -m window --focus east
+
+        cmd + shift - h : yabai -m window --swap west
+        cmd + shift - j : yabai -m window --swap south
+        cmd + shift - k : yabai -m window --swap north
+        cmd + shift - l : yabai -m window --swap east
+
+        cmd - return : kitty -d ~
+        cmd - 0x2C : kitty -d ~ -e fish -C 'lf'
+        cmd + shift - 0x2C : open ~
+
+        cmd + shift - b : yabai -m space --balance
+        cmd + shift - y : launchctl kickstart -k gui/501/org.nixos.yabai
+        cmd + shift - 0x2A : yabai -m window --toggle float
+        cmd + shift - p : qbpm launch personal
+        cmd + shift - u : qbpm launch university
+        cmd + shift - g : qbpm launch gaming
+        cmd + shift - n : qbpm launch mod
+      '';
+    };
+    yabai = {
+      enable = true;
+      config = {
+        mouse_follows_focus = "on";
+        focus_follows_mouse = "autoraise";
+        window_topmost = "on";
+        mouse_modifier = "cmd";
+        mouse_action1 = "move";
+        mouse_action2 = "resize";
+        mouse_drop_action = "swap";
+        layout = "bsp";
+        window_gap = 16;
+      };
+      package = pkgs.yabai.overrideAttrs (oldAttrs: {
+        version = "4.0.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "koekeishiya";
+          repo = "yabai";
+          rev = "910fb43b57866dc6eaa000331bb1b77d91bf245b";
+          sha256 = "9YYYUzNCXEOLOlBmVtPOMW0ikFCvGCl9YnK6NPue3kA=";
+        };
+        # TODO: please don't look too close at this :see_no_evil:
+        prePatch = ''
+          substituteInPlace makefile \
+              --replace xcrun 'SDKROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk /usr/bin/xcrun'
+        '';
+      });
+    };
+  };
+}

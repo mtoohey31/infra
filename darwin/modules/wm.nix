5bae55b039eb0db5af7409dc7558ef2cbb245df0
Matthew Toohey <contact@mtoohey.com>
2022-05-14 13:51:04 -0400

    improve yabai swap commands

--- darwin/modules/wm.nix
+++ darwin/modules/wm.nix
@@ -11,47 +11,48 @@ with lib; {
 
   config = mkIf cfg.enable {
     launchd.user.agents.skhd.serviceConfig.EnvironmentVariables.SHELL = "${pkgs.bash}/bin/bash";
-    services = {
-      skhd = {
-        enable = true;
-        skhdConfig = ''
-          cmd - h : yabai -m window --focus west || yabai -m display --focus west
-          cmd - j : yabai -m window --focus south || yabai -m display --focus south
-          cmd - k : yabai -m window --focus north || yabai -m display --focus north
-          cmd - l : yabai -m window --focus east || yabai -m display --focus east
+    services = let mod = "cmd"; in
+      {
+        skhd = {
+          enable = true;
+          skhdConfig = ''
+            ${mod} - h : yabai -m window --focus west || yabai -m display --focus west
+            ${mod} - j : yabai -m window --focus south || yabai -m display --focus south
+            ${mod} - k : yabai -m window --focus north || yabai -m display --focus north
+            ${mod} - l : yabai -m window --focus east || yabai -m display --focus east
 
-          cmd + shift - h : yabai -m window --swap west
-          cmd + shift - j : yabai -m window --swap south
-          cmd + shift - k : yabai -m window --swap north
-          cmd + shift - l : yabai -m window --swap east
+            ${mod} + shift - h : (yabai -m window --swap west && sleep 0.1 && yabai -m window --focus mouse && sleep 0.1 && yabai -m window --focus recent) || (yabai -m display --focus west && sleep 0.1 && yabai -m window --swap recent && yabai -m window --focus recent)
+            ${mod} + shift - j : (yabai -m window --swap south && sleep 0.1 && yabai -m window --focus mouse && sleep 0.1 && yabai -m window --focus recent) || (yabai -m display --focus south && sleep 0.1 && yabai -m window --swap recent && yabai -m window --focus recent)
+            ${mod} + shift - k : (yabai -m window --swap north && sleep 0.1 && yabai -m window --focus mouse && sleep 0.1 && yabai -m window --focus recent) || (yabai -m display --focus north && sleep 0.1 && yabai -m window --swap recent && yabai -m window --focus recent)
+            ${mod} + shift - l : (yabai -m window --swap east && sleep 0.1 && yabai -m window --focus mouse && sleep 0.1 && yabai -m window --focus recent) || (yabai -m display --focus east && sleep 0.1 && yabai -m window --swap recent && yabai -m window --focus recent)
 
-          cmd + shift - tab : yabai -m window --toggle float
-          cmd - return : osascript -e 'if application "iTerm2" is running then' -e 'tell application "iTerm2" to create window with default profile' -e 'else' -e 'tell application "iTerm2" to activate' -e 'end if'
+            ${mod} + shift - tab : yabai -m window --toggle float
+            ${mod} - return : osascript -e 'if application "iTerm2" is running then' -e 'tell application "iTerm2" to create window with default profile' -e 'else' -e 'tell application "iTerm2" to activate' -e 'end if'
 
-          cmd + shift - b : yabai -m space --balance
-          cmd + shift - y : launchctl kickstart -k gui/501/org.nixos.yabai
+            ${mod} + shift - b : yabai -m space --balance
+            ${mod} + shift - y : launchctl kickstart -k gui/501/org.nixos.yabai
 
-          cmd - 0x2C : osascript -e 'tell application "iTerm2" to create window with default profile command "fish -C lf"'
-          cmd + shift - 0x2C : open ~
-        '';
-      };
-      yabai = {
-        enable = true;
-        enableScriptingAddition = true;
-        config = {
-          mouse_follows_focus = "on";
-          focus_follows_mouse = "autoraise";
-          window_topmost = "on";
-          window_shadow = "float";
-          mouse_modifier = "cmd";
-          mouse_action1 = "move";
-          mouse_action2 = "resize";
-          mouse_drop_action = "swap";
-          layout = "bsp";
-          window_gap = 16;
+            ${mod} - 0x2C : osascript -e 'tell application "iTerm2" to create window with default profile command "fish -C lf"'
+            ${mod} + shift - 0x2C : open ~
+          '';
         };
-        package = pkgs.yabai;
+        yabai = {
+          enable = true;
+          enableScriptingAddition = true;
+          config = {
+            mouse_follows_focus = "on";
+            focus_follows_mouse = "autoraise";
+            window_topmost = "on";
+            window_shadow = "float";
+            mouse_modifier = mod;
+            mouse_action1 = "move";
+            mouse_action2 = "resize";
+            mouse_drop_action = "swap";
+            layout = "bsp";
+            window_gap = 16;
+          };
+          package = pkgs.yabai;
+        };
       };
-    };
   };
 }

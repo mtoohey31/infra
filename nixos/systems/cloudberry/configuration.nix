8ad417671c8de624e1c52130e059964d05d5257a
Matthew Toohey <contact@mtoohey.com>
2022-05-24 21:30:27 -0400

    home-assistant works on cloudberry

--- nixos/systems/cloudberry/configuration.nix
+++ nixos/systems/cloudberry/configuration.nix
@@ -30,60 +30,70 @@ inputs:
         { inherit lib pkgs; }).boot.initrd.availableKernelModules);
   };
 
-  sops.secrets.cloudflare_config.sopsFile = ./secrets.yaml;
-  services.caddy = {
-    enable = true;
-    package = pkgs.caddy-cloudflare;
-    extraConfig = config.sops.secrets.cloudflare_config.path;
+  sops.secrets.home_assistant_secrets = {
+    owner = config.users.users.hass.name;
+    inherit (config.users.users.hass) group;
+    sopsFile = ./secrets.yaml;
   };
-
+  systemd.services.home-assistant.preStart = ''
+    rm -f ${config.services.home-assistant.configDir}/secrets.yaml
+    ln -s ${config.sops.secrets.home_assistant_secrets.path} \
+      ${config.services.home-assistant.configDir}/secrets.yaml
+  '';
   services.home-assistant = {
     enable = true;
+    extraComponents = [
+      "backup"
+      "default_config"
+      "esphome"
+      "flic"
+      "homekit"
+      "homekit_controller"
+      "met"
+      "tplink"
+      "wake_on_lan"
+    ];
+    openFirewall = true;
     config = {
       default_config = { };
+      met = { };
       tplink = {
         discovery = false;
         switch = [
-          { host = "192.168.86.21"; }
-          { host = "192.168.86.165"; }
-          { host = "192.168.86.166"; }
-          { host = "192.168.86.201"; }
+          {
+            entity_id = "switch.lamp";
+            name = "Lamp";
+            host = "!secret lamp_ip";
+          }
+          {
+            entity_id = "switch.lights";
+            name = "Lights";
+            host = "!secret lights_ip";
+          }
+          {
+            entity_id = "switch.chandelier";
+            name = "Chandelier";
+            host = "!secret chandelier_ip";
+          }
         ];
       };
       binary_sensor = [{
         platform = "flic";
-        host = "192.168.86.24";
+        host = "!secret flic_ip";
       }];
-      camera = {
-        platform = "generic";
-        name = "!secret traffic_cam_name";
-        still_image_url = "!secret traffic_cam_url";
-      };
-      sensor = [
-        { platform = "google_wifi"; }
+      wake_on_lan = { };
+      switch = [
         {
-          platform = "waze_travel_time";
-          name = "Work Travel Time";
-          origin = "!secret waze_origin";
-          destination = "!secret waze_destination";
-          region = "US";
-          avoid_ferries = true;
-          avoid_toll_roads = true;
-          avoid_subscription_roads = true;
+          platform = "wake_on_lan";
+          name = "NAS";
+          mac = "!secret server_mac";
+          host = "!secret server_ip";
+          turn_off.service = "shell_command.turn_off_nas";
         }
       ];
-      wake_on_lan = { };
-      switch = [{
-        platform = "wake_on_lan";
-        name = "NAS";
-        mac = "!secret server_mac_address";
-        host = "!secret server_ip_address";
-        turn_off = { service = "shell_command.turn_off_nas"; };
-      }];
-      shell_command = { turn_off_nas = "!secret server_off_command"; };
+      shell_command.turn_off_nas = "!secret server_off_command";
       homekit = [{
         name = "Home Assistant Bridge";
-        port = 51827;
         filter = {
           include_entities = [
             "switch.chandelier"
@@ -93,29 +103,155 @@ inputs:
           ];
         };
         entity_config = {
-          "switch.chandelier" = { name = "Chandelier"; };
-          "switch.lamp" = { name = "Lamp"; };
-          "switch.outside_lights" = { name = "Lights"; };
-          "switch.nas" = { name = "NAS"; };
+          "switch.chandelier".name = "Chandelier";
+          "switch.lamp".name = "Lamp";
+          "switch.outside_lights".name = "Lights";
+          "switch.nas".name = "NAS";
         };
       }];
       homeassistant = {
         customize = {
-          # TODO
+          "switch.nas".icon = "mdi:nas";
+          "switch.chandelier".icon = "mdi:ceiling-light";
+          "switch.lamp".icon = "mdi:floor-lamp";
+          "switch.outside_lights".icon = "mdi:lightbulb-group";
         };
       };
-      group = {
-        # TODO
-      };
-      automation = {
-        # TODO
-      };
-      script = {
-        # TODO
-      };
-      scene = {
-        # TODO
-      };
+      automation = [
+        {
+          action = [{
+            service = "switch.toggle";
+            target.entity_id = "switch.chandelier";
+          }];
+          alias = "Toggle Chandelier";
+          condition = [ ];
+          description = "";
+          id = "1603767740442";
+          mode = "single";
+          trigger = [
+            {
+              event_data = {
+                button_name = "!secret wall_flic";
+                click_type = "single";
+              };
+              event_type = "flic_click";
+              platform = "event";
+            }
+            {
+              event_data = {
+                button_name = "!secret desk_flic";
+                click_type = "single";
+              };
+              event_type = "flic_click";
+              platform = "event";
+            }
+          ];
+        }
+        {
+          action = [{
+            service = "switch.toggle";
+            target.entity_id = "switch.lamp";
+          }];
+          alias = "Toggle Lamp";
+          condition = [ ];
+          description = "";
+          id = "1603768200755";
+          mode = "single";
+          trigger = [{
+            event_data = {
+              button_name = "!secret lamp_flic";
+              click_type = "single";
+            };
+            event_type = "flic_click";
+            platform = "event";
+          }];
+        }
+        {
+          action = [
+            {
+              service = "switch.turn_off";
+              target.entity_id = "switch.chandlier";
+            }
+            {
+              service = "switch.turn_off";
+              target.entity_id = "switch.lamp";
+            }
+          ];
+          alias = "Inside Lights Off";
+          condition = [ ];
+          description = "";
+          id = "1603767770535";
+          mode = "single";
+          trigger = [{
+            event_data = {
+              button_name = "!secret wall_flic";
+              click_type = "hold";
+            };
+            event_type = "flic_click";
+            platform = "event";
+          }];
+        }
+        {
+          action = [{
+            service = "switch.turn_on";
+            target.entity_id = "switch.outside_lights";
+          }];
+          alias = "Outside Lights On";
+          condition = [ ];
+          description = "";
+          id = "1616991316310";
+          mode = "single";
+          trigger = [{
+            event = "sunset";
+            platform = "sun";
+          }];
+        }
+        {
+          action = [{
+            service = "switch.turn_off";
+            target.entity_id = "switch.outside_lights";
+          }];
+          alias = "Outside Lights Off";
+          condition = [ ];
+          description = "";
+          id = "1616991271753";
+          mode = "single";
+          trigger = [{
+            at = "23:30:00";
+            platform = "time";
+          }];
+        }
+        {
+          action = [{
+            service = "switch.turn_on";
+            target.entity_id = "switch.nas";
+          }];
+          alias = "Server On";
+          condition = [ ];
+          description = "";
+          id = "1622914949754";
+          mode = "single";
+          trigger = [{
+            at = "7:00";
+            platform = "time";
+          }];
+        }
+        {
+          action = [{
+            service = "switch.turn_off";
+            target.entity_id = "switch.nas";
+          }];
+          alias = "Server Off";
+          condition = [ ];
+          description = "";
+          id = "1622915011141";
+          mode = "single";
+          trigger = [{
+            at = "00:00";
+            platform = "time";
+          }];
+        }
+      ];
     };
   };
 }

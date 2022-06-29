36c6480d668286e14c7530a0afe9393773b6ac4a
Matthew Toohey <contact@mtoohey.com>
2022-06-28 21:56:52 -0400

    bump qutebrowser darwin version

--- pkgs/applications/networking/browsers/qutebrowser/default.nix
+++ pkgs/applications/networking/browsers/qutebrowser/default.nix
@@ -4,18 +4,18 @@ if stdenv.hostPlatform.isDarwin then
   stdenv.mkDerivation
   rec {
     pname = "qutebrowser";
-    version = "2.5.1";
-    sourceRoot = "${pname}.app";
+    version = "2.5.2";
+    sourceRoot = "qutebrowser.app";
     src = builtins.fetchurl {
-      url = "https://github.com/${pname}/${pname}/releases/download/v${version}/${pname}-${version}.dmg";
-      sha256 = "1Pda2gzmGgiN6f/0K4sOSUav1HvLTlzrEJd8fT9lgBw=";
+      url = "https://github.com/qutebrowser/qutebrowser/releases/download/v${version}/qutebrowser-${version}.dmg";
+      sha256 = "0fdv0vk5x2lq50jhcaxs7gs5fgysfqjrvr1iddl06la8wfnwfllw";
     };
     buildInputs = [ undmg ];
     installPhase = ''
-      mkdir -p "$out/Applications/${pname}.app"
-      cp -R . "$out/Applications/${pname}.app"
-      chmod +x "$out/Applications/${pname}.app/Contents/MacOS/${pname}"
-      mkdir "$out/bin"
-      ln -s "$out/Applications/${pname}.app/Contents/MacOS/${pname}" "$out/bin/${pname}"
+      mkdir -p $out/Applications/qutebrowser.app
+      cp -R . $out/Applications/qutebrowser.app
+      chmod +x $out/Applications/qutebrowser.app/Contents/MacOS/qutebrowser
+      mkdir $out/bin
+      ln -s $out/Applications/qutebrowser.app/Contents/MacOS/qutebrowser $out/bin/qutebrowser
     '';
   } else qutebrowser

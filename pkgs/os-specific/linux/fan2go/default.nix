7cb07608a92693f4185cef7565b833326619991c
Matthew Toohey <contact@mtoohey.com>
2022-07-02 12:33:40 -0400

    clean up fan2go derivation

--- pkgs/os-specific/linux/fan2go/default.nix
+++ pkgs/os-specific/linux/fan2go/default.nix
@@ -6,9 +6,9 @@ buildGo118Module rec {
   src = fan2go-src;
   preBuild = ''
     substituteInPlace vendor/github.com/md14454/gosensors/gosensors.go \
-      --replace '// #include <sensors/sensors.h>' '// #include "${lm_sensors}/include/sensors/sensors.h"' \
-      --replace '"/etc/sensors3.conf"' '"/nix/store/ag7sxfsw6kqfhchwav1vwmaphiyzmijf-lm-sensors-3.6.0/etc/sensors3.conf"'
+      --replace '"/etc/sensors3.conf"' '"${lm_sensors}/etc/sensors3.conf"'
   '';
+  CGO_CFLAGS = "-I ${lm_sensors}/include";
   CGO_LDFLAGS = "-L ${lm_sensors}/lib";
-  vendorSha256 = "Cp7daTPxRy2BkgqX4njoi49QDPjSVHZpH3p8Dd+E8Ic=";
+  vendorSha256 = "Yl/QO4nSaNGZkeFVFXdz6UBT0gRc2ddAPhw8iLojVsw=";
 }

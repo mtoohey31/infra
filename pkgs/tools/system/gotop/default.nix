12e1c5caccce893254296a9592650500d3179f99
Matthew Toohey <contact@mtoohey.com>
2022-07-09 12:10:34 -0400

    only patch gotop on darwin

--- pkgs/tools/system/gotop/default.nix
+++ pkgs/tools/system/gotop/default.nix
@@ -1,10 +1,12 @@
-{ buildGoModule, gotop }:
+{ buildGoModule, gotop, stdenv }:
 
-gotop.override {
-  buildGoModule = args: buildGoModule (args // {
-    meta = args.meta // { broken = false; };
-    patches = [ ./0001-fix-bump-gopsutil-to-fix-darwin-compatability.patch ];
-    vendorSha256 = "G/hFpiOMgJfUiJfplpgtkcJKZMqM7WodrEl5wE0jgbY=";
-  });
-}
-
+if stdenv.hostPlatform.isDarwin then
+  gotop.override
+  {
+    buildGoModule = args: buildGoModule (args // {
+      meta = args.meta // { broken = false; };
+      patches = [ ./0001-fix-bump-gopsutil-to-fix-darwin-compatability.patch ];
+      vendorSha256 = "G/hFpiOMgJfUiJfplpgtkcJKZMqM7WodrEl5wE0jgbY=";
+    });
+  }
+else gotop
